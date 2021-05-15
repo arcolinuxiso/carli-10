8a3e54e56d2e4d1308beb3538411f172a2e5aa81
Erik Dubois <erik.dubois@gmail.com>
2021-05-15 09:47:31 +0200

    archiso version changed

--- installation-scripts/40-build-the-iso-local-again.sh
+++ installation-scripts/40-build-the-iso-local-again.sh
@@ -34,7 +34,7 @@ echo
 	isoLabel='carli-'$carliVersion'-x86_64.iso'
 
 	# setting of the general parameters
-	archisoRequiredVersion="archiso 53-1"
+	archisoRequiredVersion="archiso 54-1"
 	buildFolder=$HOME"/carli-build"
 	outFolder=$HOME"/Carli-Out"
 	archisoVersion=$(sudo pacman -Q archiso)
