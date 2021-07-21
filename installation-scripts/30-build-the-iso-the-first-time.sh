d034bc8baf80230c49a379e43ec6d405312b8cc8
Erik Dubois <erik.dubois@gmail.com>
2021-07-21 09:32:14 +0200

    change version for new config

--- installation-scripts/30-build-the-iso-the-first-time.sh
+++ installation-scripts/30-build-the-iso-the-first-time.sh
@@ -29,12 +29,12 @@ echo
 	desktop="plasma"
 	dmDesktop="plasma"
 
-	carliVersion='v21.07.1'
+	carliVersion='v21.07.2'
 
 	isoLabel='carli-'$carliVersion'-x86_64.iso'
 
 	# setting of the general parameters
-	archisoRequiredVersion="archiso 56-1"
+	archisoRequiredVersion="archiso 56.1-1"
 	buildFolder=$HOME"/carli-build"
 	outFolder=$HOME"/Carli-Out"
 	archisoVersion=$(sudo pacman -Q archiso)
